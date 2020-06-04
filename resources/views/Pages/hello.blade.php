e7f0728cbb43ca44c7a2d4fd093ab020fe1786d0
ikramSB <ikram.sabrahou@gmail.com>
2020-06-04 23:23:21 +0100

    init
    Signed-off-by: ikramSB <ikram.sabrahou@gmail.com>

---
+++ resources/views/Pages/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name','MyDOC')}}</title>
+    </head>
+    <body>
+       <h1> this is me </h1>
+       <p> hello </p>
+    </body>
+</html>
